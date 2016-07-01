210be65119528035a5dd701e7a92a4dcfaf9f714
GreenJoey <esyray15@gmail.com>
2016-07-01 15:37:48 +0530

    Add files via upload

---
+++ py3/uri/1042.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+ins=map(int,input().split())
+s=list(ins)
+for a in sorted(s):
+ print(a)
+print()
+for a in s:
+ print(a)
---
+++ py3/uri/1052.py
@@ -0,0 +1,2 @@
+mnth=["January","February","March","April","May","June","July","August","September","October","November","December"]
+print(mnth[int(input())-1])
