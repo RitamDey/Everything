1287293ff796a7eb00b53191dc0ed96022336ef5
Joe Green <esyray15@gmail.com>
2016-07-01 16:50:01 +0530

    Add files via upload

---
+++ C/CodeChef/revnum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int t;
+  scanf("%i",&t);
+  for(int a=1,n;a<=t&&scanf("%i",&n);a++)
+  {
+    while(n)
+    {
+      int x=n%10;
+      n/=10;
+      printf("%i",x);
+    }
+    printf("\n");
+  }
+  return 0;
+}
