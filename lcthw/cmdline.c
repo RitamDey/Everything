25737567e64eba4cc4d21b6b05a2783591e1496c
Joe Green <esyray15@gmail.com>
2016-07-01 16:37:47 +0530

    Add files via upload

---
+++ lcthw/cmdline.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+ printf("argc value is %i\n",argc);
+ return 0;
+}
---
+++ lcthw/first.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+ int dist=100;
+ printf("You are %i miles away.\n",dist);
+ return 0;
+}
