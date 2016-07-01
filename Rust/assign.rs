71bad6246e3bdd41111627ae33bf3cc5bf6f0b50
Joe Green <esyray15@gmail.com>
2016-07-01 16:35:53 +0530

    Add files via upload

---
+++ Rust/assign.rs
@@ -0,0 +1,4 @@
+fn main() {
+ let x: i32=5;
+ println!("The value of X {}",x);
+}
---
+++ Rust/shadow.rs
@@ -0,0 +1,10 @@
+fn main(){
+ let mut x=8;
+ println!("{}",x);
+ let x=12;
+ println!("{}",x);
+ let y=15;
+ println!("{}",y);
+ let y="Hello World";
+ println!("{}",y);
+}
