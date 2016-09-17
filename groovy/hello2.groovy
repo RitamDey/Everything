77637ec8c85c39339dc879338a526fdf992e0a45
Joe Green <GreenJoey@users.noreply.github.com>
2016-09-17 20:23:09 +0530

    Update hello2.groovy

--- groovy/hello2.groovy
+++ groovy/hello2.groovy
@@ -1,8 +1,17 @@
 class Greet{
+ 
  def name
- Greet(who){name=who[0].toUpperCase()+who[1..-1]}
- def salute(){println "Hello $name!"}
+ 
+ Greet(who){
+  name=who[0].toUpperCase()+who[1..-1]
+ }
+ 
+ def salute(){
+  println "Hello $name!"
+ }
 }
+
+
 g=new Greet("World")
 g.salute()
 
