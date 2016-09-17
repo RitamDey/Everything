0962637572093d8f15ebbe353212bf6108a8c3d3
GreenJoey <esyray15@gmail.com>
2016-09-18 03:05:15 +0530

    Added solution to Stacks And Queue problem

---
+++ py3/hackerank/queues stacks.py
@@ -0,0 +1,36 @@
+class Solution:
+  
+  def __init__(self):
+    self._stack = []
+    self._queue = []
+  
+  def pushCharacter(self, ch):
+    self._stack.append(ch)
+  
+  def enqueueCharacter(self,ch):
+    self._queue.append(ch)
+  
+  def popCharacter(self):
+    return self._stack.pop()
+
+  def dequeueCharacter(self):
+    return self._queue.pop(0)
+
+
+s=input()
+obj=Solution()   
+
+l=len(s)
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+    
+isPalindrome=True
+for i in range(l // 2):
+    if obj.popCharacter()!=obj.dequeueCharacter():
+        isPalindrome=False
+        break
+if isPalindrome:
+    print("The word, "+s+", is a palindrome.")
+else:
+    print("The word, "+s+", is not a palindrome.")
