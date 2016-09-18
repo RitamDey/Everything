47ec12dffa48b821e5d83e50673f7257b61cf771
GreenJoey <esyray15@gmail.com>
2016-09-19 04:18:24 +0530

    Added the solution of the input problem

---
+++ py3/hackerank/input_prob.py
@@ -0,0 +1,5 @@
+x,k = map(int, raw_input().split()) #Get the x's value and the k's value
+
+result = input() #Evaluate the result of the given polynomial
+
+print(result == k) #Print the boolean result of the evaluated result and the given k
