710ca3da61ce3270447d5a4adead477b7d65c82b
GreenJoey <esyray15@gmail.com>
2016-10-18 17:51:53 +0530

    A sample for..in loop

---
+++ Swift/loop.swift
@@ -0,0 +1,6 @@
+let values = ["Hello, ", "This ","is ","a ","stupid ","string"] //Declare a sample array
+
+//A sample for..in loop to print all the strings in values
+for value in values{
+    print(value)
+}
