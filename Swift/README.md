6c0e856eff382b452f3ecbc38b1132154abdfaba
GreenJoey <esyray15@gmail.com>
2016-11-23 19:40:54 +0530

    Deleted the swift files

--- Swift/README.md
+++
@@ -1,5 +0,0 @@
-#Swift Programs
-
-All the files in the foder are **valid Swift** programs 
-Run it using `swift <program>.swift`
-
--- Swift/loop.swift
+++
@@ -1,6 +0,0 @@
-let values = ["Hello, ", "This ","is ","a ","stupid ","string"] //Declare a sample array
-
-//A sample for..in loop to print all the strings in values
-for value in values{
-    print(value)
-}
