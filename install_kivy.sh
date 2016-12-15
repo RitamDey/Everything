d96f8a770f1742dcb41adae6bcc7d0aa04f630fb
Joe Green <GreenJoey@users.noreply.github.com>
2016-12-15 09:51:33 +0530

    Delete install_kivy.sh

--- install_kivy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Enter the root password when asked"
-
-su -c "pip install cython && pip install hg+http://bitbucket.org/pygame/pygame && pip install kivy"
-
-echo "Kivy is installed"
