d804000ca05089440b8468667e8c8b6d1cd6b407
sTux <esyary15@gmail.com>
2017-03-05 19:24:23 +0530

    Added cmd flags and option to dump commands to stdout.Also script now dumps pip commands

--- requirements/generate_req.py
+++ requirements/generate_req.py
@@ -1,14 +1,36 @@
-from sys import argv, exit
+from sys import argv, exit, stdout
 from pip import get_installed_distributions
+
+
+def parse_arguments():
+    """
+    Searches argv for flags
+    and sets some global variables
+    """
+    global USE_VERSION
+    global files
+    files = list()
+    USE_VERSION = False
+    if '--set-min' in argv:
+        USE_VERSION = True
+        argv.remove('--set-min') # Remove them so they don't intergere with others
+    if '--dump' in argv:
+        files.append(stdout)
+        argv.remove('--dump')
+    files.extend(argv[1:])
 
 
 def get_pkgs() -> list:
     """
     Get a list of names of modules installed
     """
+    global USE_VERSION
     pkgs = []
     for mod in get_installed_distributions():
-        pkgs.append(mod.project_name)
+        if USE_VERSION:
+            pkgs.append(f'{mod.project_name}>={mod.parsed_version}')
+        else:
+            pkgs.append(mod.project_name)
     pkgs.sort()  # Because pip freeze returns in sorted order
     print(len(pkgs))
     return pkgs
@@ -22,7 +44,7 @@ def open_file(file_name: str, mode: str):
         if 'txt' in file_name.split('.'):
             return open(file_name, mode)
         else:
-            if 'y' == input('Not a txt file: {file_name}. Continue?[y\\n]'):
+            if 'y' == input(f'Not a txt file: {file_name}. Continue?[y\\n]'):
                 return open(file_name, mode)
     except FileNotFoundError:
         return open(file_name, 'w')
@@ -32,8 +54,11 @@ def get_files():
     """
     Returns file objects for all the files given
     """
-    for fname in argv[1:]:
-        fin =  open_file(fname, 'r+')
+    global files
+    for fname in files:
+        if fname == stdout:
+            yield fname
+        fin = open_file(fname, 'r+')
         yield fin
         fin.close()
 
@@ -43,10 +68,11 @@ def get_files():
 def process():
     fout = (yield)
     for module in get_pkgs():
-        print(module, file=fout)
+        print(f'pip install --upgrade {module}', file=fout)
 
 
 if __name__ == '__main__':
+    parse_arguments()
     line_processor = process()
     next(line_processor)
     for fname in get_files():
