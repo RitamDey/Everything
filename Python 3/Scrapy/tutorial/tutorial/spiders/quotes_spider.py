d79ae92a6edbae8adb9424f9cddd6b9ea5b53f46
sTux <esyary15@gmail.com>
2017-03-07 23:09:52 +0530

    Added a example of getting and scraping next pages

--- Python 3/Scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ Python 3/Scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -5,15 +5,17 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
             'http://quotes.toscrape.com/page/1',
-            'http://quotes.toscrape.com/page/2',
-        ]
+    ]
 
     def parse(self, response):
         for quote in response.css('div.quote'):
                 text = str(quote.css('span.text::text').extract_first())[1:-1],
                 author =  quote.css('span small::text').extract_first(),
-                # 'tags': quote.css('div.tags a.tag::text').extract(),
-
                 yield {
                     'quote': f'{text[0]} \n by {author[0]}',
                 }
+
+                nxt = response.css('li.next a::attr(href)').extract_first()
+                if nxt is not None:
+                    nxt = response.urljoin(nxt)
+                    yield scrapy.Request(nxt, callback=self.parse)
