d621a355a37500c30abb5994cd830476e654371e
sTux <esyary15@gmail.com>
2017-03-13 23:38:36 +0530

    Added tests for add, mul and divide

--- python/Byterun/vm_test.py
+++ python/Byterun/vm_test.py
@@ -1,22 +1,65 @@
-from interpreter import Interpreter
+from vm import VM
 
 
-def inter_test():
-    obj = Interpreter()
+obj = VM()
 
+
+def vm_mul_test():
+    global obj
     bytecode = {
-        'instructions':[('PUSH_VALUE', 1),
-                        ('PUSH_VALUE', 2),
-                        ('ADD_TWO_NUMBERS', None),
-                        ('PUSH_VALUE', 3),
-                        ('ADD_TWO_NUMBERS', None),
-                        ('PRINT_ANSWER', None)
-                    ],
-        'values':[0, 1, 2, 7]
+        'instructions': [
+            ('PUSH_VALUE', 1),
+            ('PUSH_VALUE', 2),
+            ('MULTIPLY', None),
+            ('PUSH_VALUE', 3),
+            ('PUSH_VALUE', 4),
+            ('MULTIPLY', None),
+            ('GET_RESULT', None)
+        ],
+
+        'values': [0, 1, 2, 3, 4, ]
     }
+
+    print("Running multiplication test 1*2*3*4", end=" ")
 
     obj.run_it(bytecode)
 
 
+def vm_add_test():
+    global obj
+    bytecode = {
+        'instructions': [
+            ('PUSH_VALUE', 0),
+            ('PUSH_VALUE', 1),
+            ('PUSH_VALUE', 2),
+            ('GET_RESULT', None)
+        ],
+
+        'values': [5, 5, 10]
+    }
+
+    print('Running test for VM addition on 5+5+10', end=" ")
+    obj.run_it(bytecode)
+
+
+
+def vm_div_test():
+    global obj
+    bytecode = {
+        'instructions': [
+            ('PUSH_VALUE', 0),
+            ('PUSH_VALUE', 1),
+            ('DIVIDE', None),
+            ('GET_RESULT', None)
+        ],
+        'values': [15, 5]
+    }
+
+    print("Running divide test 15/5", end=" ")
+    obj.run_it(bytecode)
+
+
 if __name__ == '__main__':
-    inter_test()
+    vm_add_test()
+    vm_mul_test()
+    vm_div_test()
