679d5c4c6393ee900fc4a615a0d277e520c89633
sTux <esyary15@gmail.com>
2017-03-13 22:50:05 +0530

    Added multiply divide and substract operations. Also renamed PRINT_RESULT to GET_RESULT as its appropriate. Also renamed the class to VM as its a VM. Also added comments

--- python/Byterun/interpreter.py
+++ python/Byterun/interpreter.py
@@ -1,29 +1,75 @@
-from collections import deque
+from collections import deque  # coz its faster than list for stacks
 
 
-class Interpreter:
+class VM:
+    """
+    * The main VM class.
+    * Basically a stack-based VM
+    """
+
     def __init__(self):
+        # Initialize the stack
         self.stack = deque()
 
     def PUSH_VALUE(self, value):
+        # Operation to push new values in stack
         self.stack.append(value)
 
-    def ADD_TWO_NUMBERS(self):
+    def ADD(self):
+        # Operation to pop to values from stack and push the result
         n1 = self.stack.pop()
         n2 = self.stack.pop()
         self.stack.append(n1+n2)
 
-    def PRINT_ANSWER(self):
+    def SUBSTRACT(self):
+        # Operation to pop two values fron stack and substract them
+        # The order depends on the order of how they are pushed in the stack
+        n1 = self.stack.pop()
+        n2 = self.stack.pop()
+        self.stack.append(n1-n2)
+
+    def DIVIDE(self):
+        n1 = self.stack.pop()
+        n2 = self.stack.pop()
+        try:
+            res = n1/n2
+        except ZeroDivisionError:
+            res = 0
+        self.stack.append(res)
+
+    def MULTIPLY(self):
+        n1 = self.stack.pop()
+        n2 = self.stack.pop()
+        self.stack.append(n1*n2)
+
+    def GET_RESULT(self):
+        # Get the result of the last operation
         print(self.stack.pop())
 
     def run_it(self, what_to_run):
+        # Parses instructions and runs them in order
+
+        # Get the instructions from bytecode passed
         instructions = what_to_run['instructions']
+
+        # Get the values in the bytecode
         values = what_to_run["values"]
+
+        # Loop over each instruction and process it
         for instruction in instructions:
+            # Unpack the operation and arguments of the instruction
             op, arg = instruction
+
+            # Process them
             if op == "PUSH_VALUE":
                 self.PUSH_VALUE(values[arg])
-            elif op == "PRINT_ANSWER":
-                self.PRINT_ANSWER()
-            else:
-                self.ADD_TWO_NUMBERS()
+            elif op == "GET_RESULT":
+                self.GET_RESULT()
+            elif op == "ADD":
+                self.ADD()
+            elif op == "MULTIPLY":
+                self.MULTIPLY()
+            elif op == "DIVIDE":
+                self.DIVIDE()
+            elif op == "SUBSTRACT":
+                self.SUBSTRACT()
