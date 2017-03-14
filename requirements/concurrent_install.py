4015b005f1aa468d8c9d82f37069a6fe85338e53
sTux <esyary15@gmail.com>
2017-03-14 18:20:53 +0530

    Added the parser and stack creator function

--- requirements/concurrent_install.py
+++ requirements/concurrent_install.py
@@ -1,6 +1,19 @@
 from concurrent.futures import ThreadPoolExecutor as threads
+from collections import deque
+from sys import stdin, argv
+
+
+stack = deque()
+
+def parse_file(fname=stdin):
+    for line in fname.readlines():
+        if line.startswith('#'):continue
+        stack.append(line)
+    fname.close()
 
 
 if __name__ == '__main__':
-    with threads(max_workers=4) as workers:
-        pass
+    parse_file(open(argv[1], 'r'))
+
+    for name in stack:
+        print(name)
