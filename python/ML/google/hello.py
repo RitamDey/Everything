c5d0cf70249f922dc6d3a49a8b2a9d66b6fea4dd
GreenJoey <esyray15@gmail.com>
2017-03-25 22:10:54 +0530

    Added comments

--- python/ML/google/hello.py
+++ python/ML/google/hello.py
@@ -1,10 +1,11 @@
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 
+# 0 is orange and bumpy, while 1 is apple and smooth
 
-features = [[140, 1], [130, 1], [150, 0], [170, 0]]
-labels = [0, 0, 1, 1]
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]  # Training data: features of fruits
+labels = [0, 0, 1, 1]  # Training data: labels for fruits
 
 
 clf = tree.DecisionTreeClassifier()
-clf: tree.DecisionTreeClassifier = clf.fit(features, labels)
-print(clf.predict([[150, 0]]))
+clf = clf.fit(features, labels)  # Training the classifier
+print(clf.predict([[150, 0]]))  # Give it some data to predict
