c53c098e1d70906a15bab15fcb27b0fd0965a08b
GreenJoey <esyray15@gmail.com>
2017-03-25 21:33:11 +0530

    Domz spider

--- python/scrapy/MyScrapers/MyScrapers/spiders/dmoz.py
+++ python/scrapy/MyScrapers/MyScrapers/spiders/dmoz.py
@@ -8,4 +8,19 @@ class DmozSpider(scrapy.Spider):
     start_urls = ['http://http://dmoztools.net/Computers/Algorithms//']
 
     def parse(self, response):
-        pass
+
+        # All the top-level selectors
+        NEXT_PAGE_SELECTOR = ''
+        PAGE_SELECTOR = ''
+
+        # All the selectors for the domains
+        PAGE_NAME_SELECTOR = ''
+        PAGE_URL_SELECTOR = ''
+
+        next_page = response.xpath(NEXT_PAGE_SELECTOR).extract_first()
+
+        if next_page is not None:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callable=self.parse
+            )
