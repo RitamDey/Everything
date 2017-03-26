4918a3d2a615df1d1a55adcec7e747770f7fa3f7
sTux <esyray15@gmail.com>
2017-03-26 07:21:06 +0530

    Completed Dmoz Tutorial Spider

--- python/scrapy/MyScrapers/MyScrapers/spiders/dmoz.py
+++ python/scrapy/MyScrapers/MyScrapers/spiders/dmoz.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from MyScrapers.items import DomainItem
 
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["http://dmoztools.net/Computers/Algorithms/"]
-    start_urls = ['http://http://dmoztools.net/Computers/Algorithms//']
+    start_urls = ['http://dmoztools.net/Computers/Algorithms/']
 
     def parse(self, response):
 
         # All the top-level selectors
         NEXT_PAGE_SELECTOR = ''
-        PAGE_SELECTOR = ''
+        PAGE_XPATH_SELECTOR = '//div[@id="site-list-content"][@class="results browse-content"]'
+        PAGE_CSS_SELECTOR = 'div.site-item'
 
         # All the selectors for the domains
-        PAGE_NAME_SELECTOR = ''
-        PAGE_URL_SELECTOR = ''
+        PAGE_TITLE_SELECTOR = './/div[3]/a/div/text()'
+        PAGE_DESCR_SELECTOR = './/div[3]/div/text()'
+        PAGE_URL_SELECTOR = './/div[@class="title-and-desc"]/a/@href'
 
-        next_page = response.xpath(NEXT_PAGE_SELECTOR).extract_first()
-
-        if next_page is not None:
-            yield scrapy.Request(
-                response.urljoin(next_page),
-                callable=self.parse
-            )
+        domain = DomainItem()
+        # Main loop to extract sites names
+        for site in response.xpath(PAGE_XPATH_SELECTOR).css(PAGE_CSS_SELECTOR):
+            domain['name'] = site.xpath(PAGE_TITLE_SELECTOR).extract_first()
+            domain['desc'] = site.xpath(PAGE_DESCR_SELECTOR).extract_first()
+            domain['desc'] = domain['desc'].replace('\r\n\t\t\t\r\n', '').strip()
+            domain['url'] = site.xpath(PAGE_URL_SELECTOR).extract_first()
+            yield domain
