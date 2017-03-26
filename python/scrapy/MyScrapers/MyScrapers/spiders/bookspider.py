e05b62170dd579864809e88b8d7e7162cbc680c3
sTux <esyray15@gmail.com>
2017-03-26 13:11:50 +0530

    The scraper can get book images
    Signed-off-by: sTux <esyray15@gmail.com>

--- python/scrapy/MyScrapers/MyScrapers/spiders/bookspider.py
+++ python/scrapy/MyScrapers/MyScrapers/spiders/bookspider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+
 from ..items import BookItem
 
 
@@ -13,13 +14,12 @@ class BookspiderSpider(scrapy.Spider):
     next_page_selector = '//div/ul[@class="pager"]/li[@class="next"]/a/@href'
 
     name_selector = './/h3/a/@title'
-    image__selector = './/div[@class="image_container"]/a/img[@class="thumbnail"]/@src'
+    image_selector = './/div[@class="image_container"]/a/img[@class="thumbnail"]/@src'
     book_link_selector = './/div[@class="image_container"]/a/@href'
 
     def parse(self, response):
         book = BookItem()
-        fout = open('to-scarpe.txt', 'w+')
-        
+
         # All the top-level extractions in this page
         products = response.xpath(self.book_selector)
         next_page = response.xpath(self.next_page_selector).extract_first()
@@ -27,11 +27,11 @@ class BookspiderSpider(scrapy.Spider):
         # The main loop for extraction of product information
         for product in products:
             book['name'] = product.xpath(self.name_selector).extract_first(),
-            book['picture'] = response.urljoin(product.xpath(self.image__selector).extract_first())
+            book['image_urls'] = [
+                f"http://books.toscrape.com{product.xpath(self.image_selector).extract_first()[2:]}", ]
             book['url'] = response.urljoin(product.xpath(self.book_link_selector).extract_first())
-            print(book['url'], file=fout)
+            # print(book['image_urls'])
             yield book
-        fout.close()
 
         if next_page is not None:
             yield scrapy.Request(
