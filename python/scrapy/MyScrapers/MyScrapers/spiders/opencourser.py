a44a6794a65021588b46db598854e16b1c527919
sTux <esyray15@gmail.com>
2017-03-29 22:17:29 +0530

    Rewritting using crawl spider
    Signed-off-by: sTux <esyray15@gmail.com>

--- python/scrapy/MyScrapers/MyScrapers/spiders/opencourser.py
+++ python/scrapy/MyScrapers/MyScrapers/spiders/opencourser.py
@@ -1,24 +1,2 @@
-# -*- coding: utf-8 -*-
-import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
-
-class CourseExtractor():
-
-    def extract_links(self, response):
-        course_selector = ''
-        pass
-
-
-class OpencourserSpider(CrawlSpider):
-    name = 'opencourser'
-    allowed_domains = ['http://opencourser.com/']
-    start_urls = ['http://http://opencourser.com/browse/?category=48/']
-
-    rules = (
-        # Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-        Rule(CourseExtractor(), callback='parse_item', follow=True),
-    )
-
-    def parse_item(self, response):
-        pass
+# from scrapy.spiders.crawl import CrawlSpider
+# from scrapy.
