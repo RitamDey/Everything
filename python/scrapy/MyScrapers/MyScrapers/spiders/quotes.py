77ac5b4573341bc8f037bab3e3069abefe851906
sTux <esyray15@gmail.com>
2017-03-29 23:10:26 +0530

    Completed the quote spider
    Signed-off-by: sTux <esyray15@gmail.com>

--- python/scrapy/MyScrapers/MyScrapers/spiders/quotes.py
+++ python/scrapy/MyScrapers/MyScrapers/spiders/quotes.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy import Spider, Request
 
 from ..items import QuotesItem
 
 
-class QuotesSpider(CrawlSpider):
+class QuotesSpider(Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'^page/\d*'), callback='parse_item', follow=True),
-    )
-
-    def parse_item(self, response):
-        quotes_selector = '//div[@class="row"]/div[@class="col-md-8"]/div[@class="quote"]'
-        quote_selector = './span[@class="text"]'
-        author_selector = './span[2]/small[@class="author"]'
-        author_url_selector = './span[2]/a'
+    def parse(self, response):
+        quotes_selector = '//div[@class="container"]/div[@class="row"]/div[@class="col-md-8"]/div[@class="quote"]'
+        quote_selector = './span[@class="text"]/text()'
+        author_selector = './span[2]/small[@class="author"]/text()'
+        author_url_selector = './span[2]/a/@href'
+        tags = './div[@class="tags"]/a[@class="tag"]'
 
         quote_item = QuotesItem()
 
-        for quote in quotes_selector:
-            quotes_selector['quote'] = quote.xpath(quote_selector).extract_first()[1:-1]
-            quotes_selector['author'] = quote.xpath(author_selector).extract_first()
-            quotes_selector['author_url'] = quote.xpath(author_url_selector).extract_first()
+        for quote in response.xpath(quotes_selector):
+            quote_item['quote'] = quote.xpath(quote_selector).extract_first()[1:-1]
+            quote_item['author'] = quote.xpath(author_selector).extract_first()
+            quote_item['author_url'] = response.urljoin(quote.xpath(author_url_selector).extract_first())
+
+            quote_item['tags'] = [
+                tag.xpath('./text()').extract_first().capitalize() \
+                for tag in quote.xpath(tags)
+                ]
 
             yield quote_item
+
+        next_link = '//div[@class="container"]/div[@class="row"]/div[@class="col-md-8"]/nav/ul[' \
+                    '@class="pager"]/li[@class="next"]/a/@href '
+
+        yield Request(
+            response.urljoin(response.xpath(next_link).extract_first()),
+            callback=self.parse
+        )
