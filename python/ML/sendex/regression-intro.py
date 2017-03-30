4a45db30c683a9637b9d04a4c11dfd2f12dc6289
sTux <esyray15@gmail.com>
2017-03-30 19:31:22 +0530

    Clean up

--- python/ML/sendex/regression-intro.py
+++ python/ML/sendex/regression-intro.py
@@ -1,7 +1,8 @@
-import pandas as pd
-import quandl
+# import pandas as pd
+import Quandl
+from math import ceil
 
-df = quandl.get('WIKI/GOOGL')
+df = Quandl.get('WIKI/GOOGL')
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 
@@ -10,5 +11,13 @@ df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open'] * 100
 
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna()
 
 print(df.head())
