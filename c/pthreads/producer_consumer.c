d23c9647dee98c1e4486d0df3c8e262624e6a81c
sTux <exyroy@gmail.com>
2017-04-05 22:59:34 +0530

    Added producer and consumer code

---
+++ c/pthreads/producer_consumer.c
@@ -0,0 +1,33 @@
+/**
+ * Created by sTux
+ * Dated April 5th, 2017
+ * Standard: C11 with GNU extensions
+ * Compile Flags: -std=gnu11 -Wall -l pthread -ggdb
+**/
+
+#include "stack/stack.h"
+#include <pthread.h>
+#define THREADS 5
+
+
+pthread_cond_t cond;
+pthread_mutex_t mutex;
+stack_t *stack;
+
+
+void producer() {
+    pthread_mutex_lock(&mutex);
+    char str[100];
+    printf("%s", str);
+    push(&stack, str);
+    pthread_mutex_unlock(&mutex);
+}
+
+
+void consumer() {
+    pthread_mutex_lock(&mutex);
+    char *str = NULL;
+    pop(&str);
+    printf("Poped %s\n", str);
+    pthread_mutex_unlock(&mutex);
+}
