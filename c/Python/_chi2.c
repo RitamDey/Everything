b47d4cb571c463c4edf91de80b1c63443c2497f9
sTux <esyray15@gmail.com>
2017-04-11 18:45:17 +0530

    Completed the function

--- c/Python/_chi2.c
+++ c/Python/_chi2.c
@@ -15,3 +15,48 @@ static PyMethodDef module_methods[] = {
 
 
 
+PyMODINIT_FUNC init_chi2(void) {
+    PyObject *module = Py_InitModule("_chi2", module_methods, module_docstring);
+    if(module == NULL)
+        return;
+    
+    /* Load `numpy` functionality */
+    import_array();
+}
+
+
+static PyObject *chi2_chi2(PyObject *self, PyObject *args) {
+    double m, b;
+    PyObject *x_obj, *y_obj, *yerr_obj;
+
+    /* Parse the input table */
+    if(!PyArg_ParseTuple(args, "ddOOO", &m, &b, &x_obj, &y_obj, &yerr_obj))
+        return NULL;
+
+    /* Intercept the input object as numpy arrays */
+    PyObject *x_array = PyArray_FROM_OTF(x_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+    PyObject *y_array = PyArray_FROM_OTF(y_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+    PyObject *yerr_array = PyArray_FROM_OTF(yerr_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+
+    /* How many data points are there? */
+    int N = (int)PyArray_DIM(x_array, 0);
+
+
+    /* Call the actual C function to compute the chi-squared. */
+    double value = chi2(m, b, x, y, yerr, N);
+
+    /* Clean up objects */
+    Py_DECREF(x_array);
+    Py_DECREF(y_array);
+    Py_DECREF(yerr_array);
+
+    if(value < 0.0) {
+        PyErr_SetString(PyExc_RuntimeError, "Chi-squared returned imposible value.");
+        return NULL;
+    }
+
+    /* Build the output tuple */
+    PyObject *ret = Py_BuildValue("d", value);
+    return ret;
+}
+
