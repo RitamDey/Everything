ba14cfb9568ecd881813f04df18070a7150cd229
sTux <esyray15@gmail.com>
2017-04-12 22:35:57 +0530

    Removed useless HTML example and PHP which I don't use

--- php/hello.php
+++
@@ -1,4 +0,0 @@
-<?php
-    echo('Hello World!!!');
-    echo('<br>This is just a test script to see if PHP is installed properly or not');
-?>
--- php/run_dev_server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-read script_name
-
-echo "Running PHP development server for $script_name script\n"
-
-php -S 127.0.0.2:8000 $script_name
