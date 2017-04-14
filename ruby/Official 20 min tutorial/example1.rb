d4e7dfdd83812560524af0dd2567f73f1dbb8d80
sTux <esyray15@gmail.com>
2017-04-14 20:48:04 +0530

    Added two example Signed-off-by: sTux <esyray15@gmail.com>

---
+++ ruby/Official 20 min tutorial/example1.rb
@@ -0,0 +1,4 @@
+puts 3+2
+puts 3*2
+puts 3**2
+puts Math.sqrt(9)
---
+++ ruby/Official 20 min tutorial/example2.rb
@@ -0,0 +1,11 @@
+=begin
+    Math is a built-in module for mathematics. 
+    Modules serve two roles in Ruby. 
+    This shows one role: grouping similar methods together under a familiar name. 
+    Math also contains methods like sin() and tan().   
+=end
+
+a = 3**2
+b = 4**2
+
+puts Math.sqrt(a+b)
