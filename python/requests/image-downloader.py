c3871415b8a051a516265f05c1815024c48e05ee
sTux <esyray15@gmail.com>
2017-04-16 15:28:10 +0530

    Cleanup Signed-off-by: sTux <esyray15@gmail.com>

--- python/requests/image-downloader.py
+++ python/requests/image-downloader.py
@@ -7,16 +7,16 @@ from sys import argv
 
 parser = ArgumentParser()
 parser.add_argument('url', type=str, default="http://www.funroundup.com/wp-content/uploads/2015/12/Poonam-Pandey-hot-and-sexy-wallpapers-in-bikini-fun-roundup3.jpg")
-parser.add_argument('-o', type=str, default="image.jpeg", optional=True)
+parser.add_argument('-o', type=str, default="image.jpeg", required=True)
 args = parser.parse_args()
 
 
 # Here we use get the image from the url and exposes the raw data as seen by the socket
-obj = requests.get(url or 'http://www.funroundup.com/wp-content/uploads/2015/12/Poonam-Pandey-hot-and-sexy-wallpapers-in-bikini-fun-roundup3.jpg', stream=True)
+obj = requests.get(args.url, stream=True)
 
 
 # Open  a binary file for writing
-with open('image.jpg', 'wb+') as fd:
+with open(args.o, 'wb+') as fd:
     """
     Using Response.iter_content will handle a lot of what you would otherwise have to handle when using Response.raw directly. 
     When streaming a download, the above is the preferred and recommended way to retrieve the content. 
