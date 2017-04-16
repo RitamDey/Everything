1b80a1da85cf60067e0066ef5a9f039737248d24
sTux <esyray15@gmail.com>
2017-04-16 15:23:23 +0530

    Added argument parser for arguments Signed-off-by: sTux <esyray15@gmail.com>

--- python/requests/iter-content-example.py
+++ python/requests/iter-content-example.py
@@ -1,8 +1,18 @@
+"""
+This script is a example of downloading and saving a image from internet using requets
+"""
+from argparse import ArgumentParser
 import requests
+from sys import argv
+
+parser = ArgumentParser()
+parser.add_argument('url', type=str, default="http://www.funroundup.com/wp-content/uploads/2015/12/Poonam-Pandey-hot-and-sexy-wallpapers-in-bikini-fun-roundup3.jpg")
+parser.add_argument('-o', type=str, default="image.jpeg", optional=True)
+args = parser.parse_args()
 
 
 # Here we use get the image from the url and exposes the raw data as seen by the socket
-obj = requests.get('http://www.funroundup.com/wp-content/uploads/2015/12/Poonam-Pandey-hot-and-sexy-wallpapers-in-bikini-fun-roundup3.jpg', stream=True)
+obj = requests.get(url or 'http://www.funroundup.com/wp-content/uploads/2015/12/Poonam-Pandey-hot-and-sexy-wallpapers-in-bikini-fun-roundup3.jpg', stream=True)
 
 
 # Open  a binary file for writing
