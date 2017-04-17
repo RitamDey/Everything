889f8a0d3bc5bffbc4252a9a151e243ed9598df5
sTux <esyray15@gmail.com>
2017-04-17 16:35:13 +0530

    Added usage of Signed-off-by: sTux <esyray15@gmail.com>

---
+++ ruby/stdin-gets.rb
@@ -0,0 +1,24 @@
+user_name = ARGV.first  # get the first element from the array
+prompt = '> '
+
+
+puts "Hi #{user_name}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}"
+puts prompt
+likes = $stdin.gets.chomp  # same as gets.chomp
+
+
+puts "Where do you live #{user_name}?"
+puts prompt
+lives = $stdin.gets.chomp
+
+
+puts "What kind of computer do you have? ", prompt  # a comma for puts is like using it twice
+computer = $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}.  Not sure where that is.
+And you have a #{computer} computer.  Nice.
+"""
