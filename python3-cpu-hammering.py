9fc088397c62f337854f486eb2399664ef697f8b
sTux <esyray15@gmail.com>
2017-04-20 11:22:24 +0530

    Added python thread benchmark code Signed-off-by: sTux <esyray15@gmail.com>

---
+++ python3-cpu-hammering.py
@@ -0,0 +1,34 @@
+from threading import Thread
+from os import cpu_count
+import argparse
+
+
+def fib(n):
+    if n <= 2:
+        return 1
+    elif n == 0:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+
+def fact(n):
+    if n <= 0:
+        return 1
+    return n*fact(n-1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', type=str, default="fib")
+    parser.add_argument('number', type=int, default=50)
+    args = parser.parse_args()
+
+    if args.c == 'fact':
+        callback = fact
+    else:
+        callback = fib
+
+    for i in range(cpu_count()):
+        t = Thread(target=callback, args=(args.number,))
+        t.start()
+
