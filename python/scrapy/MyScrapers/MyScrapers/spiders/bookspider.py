548da7f53ed10ee115083040ed87f7a9da5636f6
sTux <esyray15@gmail.com>
2017-04-21 22:04:37 +0530

    Changes xpaths Signed-off-by: sTux <esyray15@gmail.com>

--- python/scrapy/MyScrapers/MyScrapers/spiders/bookspider.py
+++ python/scrapy/MyScrapers/MyScrapers/spiders/bookspider.py
@@ -13,9 +13,9 @@ class BookspiderSpider(scrapy.Spider):
     book_selector = '//article[@class="product_pod"]'
     next_page_selector = '//div/ul[@class="pager"]/li[@class="next"]/a/@href'
 
-    name_selector = './/h3/a/@title'
-    image_selector = './/div[@class="image_container"]/a/img[@class="thumbnail"]/@src'
-    book_link_selector = './/div[@class="image_container"]/a/@href'
+    name_selector = './h3/a/@title'
+    image_selector = './div[@class="image_container"]/a/img[@class="thumbnail"]/@src'
+    book_link_selector = './div[@class="image_container"]/a/@href'
 
     def parse(self, response):
         book = BookItem()
@@ -23,7 +23,7 @@ class BookspiderSpider(scrapy.Spider):
         # All the top-level extractions in this page
         products = response.xpath(self.book_selector)
         next_page = response.xpath(self.next_page_selector).extract_first()
-        
+
         # The main loop for extraction of product information
         for product in products:
             book['name'] = product.xpath(self.name_selector).extract_first(),
