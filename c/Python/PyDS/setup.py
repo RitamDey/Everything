4f061ae73e0f279aa0642323bbf19bbb7f402ab4
sTux <esyray15@gmail.com>
2017-04-26 16:34:29 +0530

    Sample C extension module Signed-off-by: sTux <esyray15@gmail.com>

---
+++ c/Python/PyDS/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup, Extension
+import numpy.distutils.misc_util
+
+
+setup(
+        ext_modules=[Extension("LinkedList", ["LinkedList.c",])],
+        include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
+)
