1c4ee19807056649ee5271c7ee906f5871cb9664
sTux <esyray15@gmail.com>
2017-05-01 21:27:19 +0530

    Added a example of tweepy Signed-off-by: sTux <esyray15@gmail.com>

---
+++ python/Social Networks/get_limited_tweets.py
@@ -0,0 +1,14 @@
+import json
+from tweepy import Cursor
+from get_tweets import make_client
+
+
+client = make_client()
+
+
+with open("tweets.json", "w+") as fout:
+    # Get 4 pages of tweets each containing 10 tweets
+    for page in Cursor(client.home_timeline, count=10).pages(4):
+        for tweet in page:
+            fout.write(json.dumps(tweet._json)+"\n\n")
+
