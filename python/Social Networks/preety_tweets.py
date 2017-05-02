269139223b02ad27dd313f8c1ba44bd47015b055
sTux <esyray15@gmail.com>
2017-05-02 08:59:13 +0530

    My first app Signed-off-by: sTux <esyray15@gmail.com>

---
+++ python/Social Networks/preety_tweets.py
@@ -0,0 +1,20 @@
+from make_client import make_client
+from tweepy import Cursor
+from sys import argv
+
+
+try:
+    tweets = argv[1]
+except IndexError:
+    tweets = 10
+
+
+client = make_client()
+for page in Cursor(client.home_timeline, count=tweets).pages(tweets):
+    for tweet in page:
+        if tweet.retweeted:
+            print("{} retweeded".format(tweet.author.name))
+        else:
+            print("{} tweeted".format(tweet.author.name))
+        print("\t{}".format(tweet.text))
+        print(tweet.created_at)
