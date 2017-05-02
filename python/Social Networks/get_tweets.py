60c80c1cbe8992401d0be8e21f5f5a1e79f3b51e
sTux <esyray15@gmail.com>
2017-05-02 08:18:09 +0530

    Added first example Signed-off-by: sTux <esyray15@gmail.com>

---
+++ python/Social Networks/get_tweets.py
@@ -0,0 +1,26 @@
+from tweepy import Cursor
+from tweepy import API
+from tweepy import OAuthHandler
+import os
+
+
+def make_client() -> API:
+    consumer_key = os.environ["CONSUMER_KEY"]
+    access_key = os.environ["ACCESS_KEY"]
+    consumer_secret = os.environ["CONSUMER_SECRET"]
+    access_secret = os.environ["ACCESS_SECRET"]
+
+    # Setup a OAuth Handler with keys
+    client = OAuthHandler(consumer_key, consumer_secret)
+    client.set_access_token(access_key, access_secret)
+
+    # Get the client using the OAuth handle created
+    client = API(client)
+
+    # Return it
+    return client
+
+
+client = make_client()
+for status in Cursor(client.home_timeline).items(10):
+    print(status.text)
