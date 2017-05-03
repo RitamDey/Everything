0d4315c4d9fd6dd150f37c51e2383c72248a477f
sTux <esyray15@gmail.com>
2017-05-03 12:03:26 +0530

    Bug Fix Signed-off-by: sTux <esyray15@gmail.com>

--- python/Social Networks/get_others_tweets.py
+++ python/Social Networks/get_others_tweets.py
@@ -1,14 +1,14 @@
 import json
 from sys import argv
 from tweepy import Cursor
-from get_tweets import make_client
+from make_client import make_client
 
 
 client = make_client()
-username = argv[2]
+username = argv[1]
 
 with open("tweets.json", "w+") as fout:
-    curr =  Cursor(client.user_timeline, screen_name=username, count=10)
+    curr = Cursor(client.user_timeline, screen_name=username, count=10)
     for page in curr.pages(10):
         for tweet in page:
             fout.write(json.dumps(tweet._json))
