2de1060155219297606e1a779278840673aed6fb
sTux <esyray15@gmail.com>
2017-05-03 12:02:43 +0530

    Analyze tweets Signed-off-by: sTux <esyray15@gmail.com>

---
+++ python/Social Networks/hashtag_counter.py
@@ -0,0 +1,33 @@
+from collections import Counter
+from argparse import ArgumentParser
+from tweepy import Cursor
+from make_client import make_client
+
+
+def get_hashtags(tweet: dict) -> list:
+    """
+    The tweet argument is a dict. So get 'entities' key or return a empty dict.
+    This is required for the second .get() to not throw a error where we try to get 'hashtags'
+    """
+    entities = tweet.get('entities', {})
+    hashtags = entities.get('hashtags', [])
+
+    return ["#"+tag['text'] for tag in hashtags]
+
+
+if __name__ == '__main__':
+    hashtags = Counter()
+    args = ArgumentParser()
+    args.add_argument("username")
+    args = args.parse_args()
+    
+    
+    client = make_client()
+    curr = Cursor(client.user_timeline, screen_name=args.username, count=10)
+    
+    for page in curr.pages(10):
+        for tweet in page:
+            hashtags.update(get_hashtags(tweet._json))
+
+    print(hashtags.most_common(20))
+
