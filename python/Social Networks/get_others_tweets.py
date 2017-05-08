dad9b0ec6e6b1eda5c57813146c02c2af97e53fe
sTux <esyray15@gmail.com>
2017-05-08 21:04:00 +0530

    Upgraded limit to 150 Signed-off-by: sTux <esyray15@gmail.com>

--- python/Social Networks/get_others_tweets.py
+++ python/Social Networks/get_others_tweets.py
@@ -8,7 +8,7 @@ client = make_client()
 username = argv[1]
 
 with open("tweets.json", "w+") as fout:
-    curr = Cursor(client.user_timeline, screen_name=username, count=10)
+    curr = Cursor(client.user_timeline, screen_name=username, count=150)
     for page in curr.pages(10):
         for tweet in page:
             fout.write(json.dumps(tweet._json))
