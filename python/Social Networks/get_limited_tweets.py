a14aa06fc5be37b7f6d08372db39efe7aa3ae917
sTux <esyray15@gmail.com>
2017-05-08 21:03:37 +0530

    Chaneged some bugs Signed-off-by: sTux <esyray15@gmail.com>

--- python/Social Networks/get_limited_tweets.py
+++ python/Social Networks/get_limited_tweets.py
@@ -10,5 +10,5 @@ with open("tweets.json", "w+") as fout:
     # Get 4 pages of tweets each containing 10 tweets
     for page in Cursor(client.home_timeline, count=10).pages(4):
         for tweet in page:
-            fout.write(json.dumps(tweet._json)+"\n\n")
+            fout.write(json.dumps(tweet._json))
 
