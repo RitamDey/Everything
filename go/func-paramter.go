86a83cdfaaba518a11e061914f406422dbf90cfd
sTux <esyray15@gmail.com>
2017-05-14 10:59:40 +0530

    Added a function creation example Signed-off-by: sTux <esyray15@gmail.com>

---
+++ go/func-paramter.go
@@ -0,0 +1,13 @@
+package main
+
+
+// Making a function
+// func <name>( <p1> <type>,....., <pn> <type>) <return type>
+func add(x int, y int) int {
+    return x+y
+}
+
+
+func main() {
+    println(add(5, 3))
+}
