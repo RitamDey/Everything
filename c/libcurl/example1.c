792266a24c9cb954f845ece615c90e73e6c91a13
sTux <esyray15@gmail.com>
2017-06-01 23:57:52 +0530

    Remove Signed-off-by: sTux <esyray15@gmail.com>

--- c/libcurl/example1.c
+++
@@ -1,9 +0,0 @@
-#include <curl.h>
-#include <stdio.h>
-
-
-int main(){
-    curl_global_init(CURL_GLOBAL_ALL);  //Initialize the libcurl
-    
-    curl_global_cleanup();  // Destroy libcurl objects
-}
