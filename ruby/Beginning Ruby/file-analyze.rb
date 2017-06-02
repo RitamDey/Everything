039e73724dee1d6aeafa695c7c769e37889246ed
sTux <esyray15@gmail.com>
2017-06-02 18:09:35 +0530

    Second example Signed-off-by: sTux <esyray15@gmail.com>

--- ruby/Beginning Ruby/file-analyze.rb
+++ ruby/Beginning Ruby/file-analyze.rb
@@ -1,9 +1,11 @@
 if __FILE__ == $0
     line_count = 0
-    word_count = 0
+    text = ""
 
-    File.open("text.txt").each { |line| line_count += 1; word_count += line.length }
+    File.open("text.txt").each do |line|
+        line_count += 1
+        text << 1
+    end
 
     $stdout.puts "Line Count is #{line_count}"
-    $stdout.puts "Word Count is #{word_count}"
 end
