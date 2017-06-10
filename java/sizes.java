c275e6a4eb4ec7b44aa2725c06c30c483a2d1630
sTux <esyray15@gmail.com>
2017-06-10 23:56:22 +0530

    Sizes examples of Java types Signed-off-by: sTux <esyray15@gmail.com>

---
+++ java/sizes.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Min value for Integer: "+Integer.MIN_VALUE);
+        System.out.println("Max value for Integer: "+Integer.MAX_VALUE);
+        System.out.println("Size of Integer: "+Integer.SIZE);
+
+        System.out.println("Min value for Long: "+Long.MIN_VALUE);
+        System.out.println("Max value for Long: "+Long.MAX_VALUE);
+        System.out.println("Size of Long: "+Long.SIZE);
+
+        System.out.println("Max value for Short: "+Short.MAX_VALUE);
+        System.out.println("Min value for Short: "+Short.MIN_VALUE);
+        System.out.println("Size of Short: "+Short.SIZE);
+    }
+}
