dd07b4524691fc40f34694920810a3061879637d
sTux <esyray15@gmail.com>
2017-06-12 22:46:29 +0530

    Functions examples Signed-off-by: sTux <esyray15@gmail.com>

---
+++ bash/Derek Banas One Video Bash/function-example.sh
@@ -0,0 +1,34 @@
+getDate() {
+    date
+
+    return
+}
+
+
+getDate
+
+
+name="Derek"
+
+
+demLocal(){
+    local name="Paul"
+    return
+}
+
+demLocal
+echo "$name"
+
+
+getSum(){
+    local num3=$1
+    local num4=$2
+    
+    local sum=$((num3+num4))
+    echo $sum
+}
+
+num1=5
+num2=6
+sum=$(getSum num1 num2)
+echo "The sum is $sum"
