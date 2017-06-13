5079e1c4279241a89d7f402eb4de4cabdcfbf8c5
sTux <esyray15@gmail.com>
2017-06-13 21:02:23 +0530

    Examples of conditionals Signed-off-by: sTux <esyray15@gmail.com>

--- bash/Derek Banas One Video Bash/conditionals-examples.sh
+++ bash/Derek Banas One Video Bash/conditionals-examples.sh
@@ -12,3 +12,22 @@ then
 else
     echo "You can't drive"
 fi
+
+
+read -p "Enter a number: " num
+if ((num == 10)); then
+    echo "Your number equals 10"
+fi
+
+if ((num > 10)); then
+    echo "It is greater than 10"
+else
+    echo "It is less then ten"
+fi
+
+
+if (( ((num % 2)) == 0 )); then
+    echo "It is even"
+else
+    echo "It is odd"
+fi
