fea4ef38a9bd738b52794bbde5b56e5e098a76d0
sTux <esyray15@gmail.com>
2017-06-15 19:50:54 +0530

    Some more examples Signed-off-by: sTux <esyray15@gmail.com>

---
+++ bash/Derek Banas One Video Bash/string-example.sh
@@ -0,0 +1,20 @@
+str1 =""
+str2="Sad"
+str3="Happy"
+
+
+if [ "$str1" ]; then
+    echo "$str1 is not null"
+fi
+
+
+if [ -z "$str1" ]; then
+    echo "$sr1 has no value"
+fi
+
+
+if [ "$str2" == "$str3" ]; then
+    echo "$str2 equals $str3"
+elif [ "$str2" != "$str3" ]; then
+    echo "$str2 is not equal to $str3"
+fi
