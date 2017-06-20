43e51484a2fa53c68f01e091ce489ddb5658fa2d
sTux <esyray15@gmail.com>
2017-06-20 21:48:15 +0530

    More examples using files Signed-off-by: sTux <esyray15@gmail.com>

--- bash/Derek Banas One Video Bash/files-checks-examples.sh
+++ bash/Derek Banas One Video Bash/files-checks-examples.sh
@@ -1,13 +1,24 @@
 file1="./test_file1"
 file2="./test_file2"
 
-
+# The -e tests if file is exists
 if [ -e "$file1" ]; then
     echo "$file1 exists"
-
+    
+    # The -f tests if file is normal
     if [ -f "$file1" ]; then
         echo "$file1 is a normal file"
     fi
+
+    # The -r tests if file is readable
+    if [ -r "$file1" ]; then
+        echo "$file1 is readable"
+    fi
+
+    # The -w tests if file is writable
+    if [ -w "$file1" ]; then
+        echo "$file1 is writable"
+    fi
+else
+    echo "$file1 doesn't exists"
 fi
-
-
