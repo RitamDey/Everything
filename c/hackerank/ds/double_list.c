a582acbdb7b8b17f753e9fdd8bb3f934437a80e1
sTux <esyray15@gmail.com>
2017-06-21 17:48:30 +0530

    Imcomplete codes and examples Signed-off-by: sTux <esyray15@gmail.com>

---
+++ c/hackerank/ds/double_list.c
@@ -0,0 +1,4 @@
+#include "double_list.h"
+
+
+void list_new(double_list_t *
---
+++ java/HackerRank/Java List.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList arr = new ArrayList<int>();
+        int x;
+        int y = in.nextInt();
+        in.nextLine();
+
+        for(int i=0; i<y; ++i) {
+            x = in.nextInt();
+            System.out.println(x);
+        }
+
+        y = in.nextInt();
+        for(int i=0; i<y; ++i) {
+
+        }
+    }
+}
