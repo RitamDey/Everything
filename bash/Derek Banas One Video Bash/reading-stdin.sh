9083aede847155b8eb05d2db37a13760416148a8
sTux <esyray15@gmail.com>
2017-06-21 21:01:55 +0530

    more example to paramter expansion Signed-off-by: sTux <esyray15@gmail.com>

--- bash/Derek Banas One Video Bash/reading-stdin.sh
+++ bash/Derek Banas One Video Bash/reading-stdin.sh
@@ -15,3 +15,37 @@ if [ $secret == "password" ]; then
 else
     echo "Wrong Password"
 fi
+
+
+# the IFS variable is used to indicate 
+# the seperator that will seperate value in stdin
+OIFS="$IFS"
+IFS=","
+
+
+read -p "Enter 2 number seperated by commas: " num1 num2
+
+
+# Using paramter expansion to subsitute whitespaces.
+num1=${num1//[[:blank:]]/}
+num2=${num2//[[:blank:]]/}
+
+
+sum=$((num1+num2))
+echo "$num1 + $num2 = $sum"
+
+
+IFS=$OIFS
+
+
+
+# Parameter exapsion examples
+name="sTux"
+echo "${name}'s toy"
+
+
+samp_string="The dog climbed the tree"
+echo "${samp_string//dog/cat}"
+
+
+echo "I am ${name:=sTux}
