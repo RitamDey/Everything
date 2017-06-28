8886632fad7b57830bce3c88958788b9e6364646
sTux <esyray15@gmail.com>
2017-06-28 22:17:11 +0530

    Completing... Signed-off-by: sTux <esyray15@gmail.com>

---
+++ ruby/HackerEarth/selection-sort.rb
@@ -0,0 +1,10 @@
+=begin
+This is the solution to the example problem in the HackerEarth's Selection Sort tutorials
+=end
+
+
+len, iter = $stdin.gets.chomp.split.map! { |x| x.to_i }
+arr = $stdin.gets.chomp.split.map! { |x| x.to_i }
+
+
+
