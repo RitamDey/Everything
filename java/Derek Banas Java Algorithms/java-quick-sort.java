4f35689637cf2a355f618647dc18befd4d890603
sTux <esyray15@gmail.com>
2017-06-28 22:17:11 +0530

    Quick Sort example Signed-off-by: sTux <esyray15@gmail.com>

---
+++ java/Derek Banas Java Algorithms/java-quick-sort.java
@@ -0,0 +1,52 @@
+import java.utils.Arrays;
+
+
+class Partitioning {
+    private static int[] array;
+    private static int size;
+
+
+    public static void main(String[] args) {
+    }
+
+
+    public void partition(int pivot) {
+        int left = -1;
+        int right = size;
+
+        while(true){
+            while(left < (this.size -1) && this.array[++left] < pivot);
+            System.out.println("Bigger value found against pivot "+pivot+" is "+this.array[left]);
+            while(right > 0 && this.array[--right] > pivot);
+            System.out.println("Smaller value found against pivot "+pivot+" is "+this.array[right]);
+
+            if(left >= right) break;
+
+            else {
+                this.swap(left, right);
+                System.out.println(this.array[left]+" was swapped for "+this.array[right]);
+            }
+
+        }
+    }
+
+
+    public void swap(int pos1, int pos2) {
+        int temp = this.array[pos1];
+        this.array[pos1] = this.array[pos2];
+        this.array[pos2] = temp;
+    }
+
+
+    public void generate() {
+        for(int i=0; i<this.size; ++i)
+            this.array[i] = (int)(Math.random()*10)+50;
+    }
+
+
+    Partitioning(int size) {
+        this.size = size;
+        this.array = new int[size];
+        this.generate();
+    }
+}
