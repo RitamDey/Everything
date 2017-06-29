62c00ae5d6d4e85cae2a1042649a00bc3ad7c677
sTux <esyray15@gmail.com>
2017-06-29 15:18:57 +0530

    Full Quick Sort Signed-off-by: sTux <esyray15@gmail.com>

--- java/Derek Banas Java Algorithms/java-quick-sort.java
+++ java/Derek Banas Java Algorithms/java-quick-sort.java
@@ -8,20 +8,40 @@ class QuickSort {
 
     public static void main(String[] args) {
         QuickSort sort = new QuickSort(10);
-        System.out.println(Arrays.toString(partion.array));
+        System.out.println(Arrays.toString(sort.array));
 
-        sort.partition(35);
+        sort.sort(0, 9);
         System.out.println(Arrays.toString(sort.array));
     }
 
+    public void sort(int left, int right) {
+        if(right - left <= 0)
+            return;  // Everything is sorted
+        
+        else {
+            // It doesn't matter  what the pivot is, but it must
+            // be a value in the array
+            int pivot = this.array[right];
 
-    public void partition(int pivot) {
-        int left = -1;
-        int right = size;
+            System.out.println("Value in right "+this.array[right]+" is made the pivot");
+            System.out.println("left="+left+" right="+right+" pivot="+pivot+"\n\n");
+
+            int pivotLocation = this.partition(left, right, pivot);
+            System.out.println("Value in left "+this.array[left]+" is made the pivot\n\n");
+
+            this.sort(left, pivotLocation - 1); // sort the left
+            this.sort(pivotLocation + 1, right); // sort the right
+        }
+    }
+
+
+    public int partition(int leftpointer, int right, int pivot) {
+        int left = leftpointer - 1;
 
         while(true){
-            while(left < (this.size -1) && this.array[++left] < pivot);
+            while(this.array[++left] < pivot);
             System.out.println("Bigger value found against pivot "+pivot+" is "+this.array[left]);
+            
             while(right > 0 && this.array[--right] > pivot);
             System.out.println("Smaller value found against pivot "+pivot+" is "+this.array[right]);
 
@@ -33,6 +53,9 @@ class QuickSort {
             }
 
         }
+
+        this.swap(leftpointer, right);
+        return leftpointer;
     }
 
 
