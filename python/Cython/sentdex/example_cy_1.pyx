8e6b3f8e34f1d79b55d6775bf8c1149e94e2f195
sTux <esyray15@gmail.com>
2017-07-12 19:51:07 +0530

    Comments and example of cdef and cpdef

--- python/Cython/sentdex/example_cy_1.pyx
+++ python/Cython/sentdex/example_cy_1.pyx
@@ -1,4 +1,18 @@
-def test(x):
+cdef int test(int x):
+    cdef int y = 0  # declare a Python Integer object in C
+    for i in range(x+1):
+        y += i
+    return y
+
+
+cpdef int c_test(int x):
+    cpdef y = test(x)  # declare a generic Python Object in C
+    return y
+
+
+# The `def` statment is used to declare a normal python function 
+# that is accessible only to Python
+def py_test(x):
     y = 0
     for i in range(x+1):
         y += i
