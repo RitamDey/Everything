71557beaebb8a2d1f248d0b58d68d764e5aa2382
sTux <esyray15@gmail.com>
2017-07-19 21:17:12 +0530

    Fixing width issue

--- python/Matplotlib/sentdex/example_3_histogram.py
+++ python/Matplotlib/sentdex/example_3_histogram.py
@@ -6,7 +6,7 @@ population_ages = [random.randint(low=20, high=130) for _ in range(40)]
 bins = list(range(0, 135, 10))
 
 
-plt.hist(population_ages, bins, histtype='bar')
+plt.hist(population_ages, bins, histtype='bar', rwidth=0.8)
 plt.xlabel('Population Ages')
 plt.ylabel('Population Group')
 plt.show()
