efff575107c7ffdc972320aeda9d7106d2f8f7ec
sTux <esyray15@gmail.com>
2017-07-27 08:29:47 +0530

    Full Example. URL doesn't works

--- python/Matplotlib/sentdex/example_8.py
+++ python/Matplotlib/sentdex/example_8.py
@@ -5,6 +5,16 @@ import urllib
 import matplotlib.dates as mdates
 
 
+def bytespdate2num(format, encoding='utf-8'):
+    strcoverter = mdates.strpdate2num(format)
+
+    def bytescoverter(b):
+        s = b.decode(encoding)
+        return strcoverter(s)
+
+    return bytescoverter
+
+
 def graph_data(stock):
     stock_price_url = 'http://chartapi.finance.yahoo.com/instrument/1.0/' + \
         stock + '/chartdata;type=quote;range=10y/csv'
@@ -12,9 +22,27 @@ def graph_data(stock):
     source_code = urllib.request.urlopen(stock_price_url).read().decode()
 
     stock_data = []
+    split_source = source_code.split('\n')
 
-    plt.xlabel('X Axis')
-    plt.ylabel('Y Axis')
+    for line in split_source:
+        split_line = line.split(',')
+        if len(split_line) == 6:
+            if 'values' not in line and 'labels' not in line:
+                stock_data.append(split_line)
+
+    date, closep, highp, lowp, openp, volume = np.loadtxt(
+        stock_data,
+        delimiter=',',
+        unpack=True,
+        # converters is a tuple where the first element
+        # is the position of element and the last is the callback
+        converters=(0, bytespdate2num('%Y%m%d'))
+    )
+
+    plt.plot(date, closep)
+
+    plt.xlabel('Date')
+    plt.ylabel('Closing Price')
     plt.title('Random Graphs from Internet')
     plt.legend()
     plt.show()
