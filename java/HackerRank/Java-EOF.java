bbf1ea2a554a1666cc94964fc2930f92519bf120
sTux <esyray15@gmail.com>
2017-08-22 10:04:04 +0530

    ..

---
+++ java/HackerRank/Java-EOF.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
