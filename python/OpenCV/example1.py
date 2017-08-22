ce9f0ee620607eb2353a2ede9bedeb38587f968a
sTux <esyray15@gmail.com>
2017-08-22 10:28:01 +0530

    Full Example

--- python/OpenCV/example1.py
+++ python/OpenCV/example1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy
 
 
 # Create a VideoCapture object and read from the input file
@@ -6,21 +7,31 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-# Define the fps to be equal to 10. Also frame size is passed.
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-
-
 # Check if camera opened successfully
 if cap.isOpened() is False:
     print("Error opening video stream")
+
+
+# Default resolution of the frame are obtained. The default resolutions are system dependent
+# We convert the resolutions from float to integer
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+
+# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+# Define the fps to be equal to 10. Also frame size is passed.
+out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
 
 
 # Read until video is completed
 while cap.isOpened():
     # Capture frame-by-frame
     ret, frame = cap.read()
+    
     if ret is True:
+
+        # Write the frame into the output file
+        out.write(frame)
 
         # Display the resulting frame
         cv2.imshow('Frame', frame)
