0373a619842fb9e7fb7823c6740800591b7202cd
sTux <esyray15@gmail.com>
2017-10-10 21:07:52 +0530

    Example of goto

---
+++ go/Control/goto.go
@@ -0,0 +1,19 @@
+package main
+
+
+import "fmt"
+
+
+func main() {
+    i := 0
+
+    goto Here
+
+
+    Here:
+        fmt.Println(i)
+        i++
+        if i <= 10 {
+            goto Here
+        }
+}
