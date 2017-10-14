e2cef4ad75fbeedf4c4f9e498d917e9edbf33f33
GreenJoey <esyray15@gmail.com>
2017-10-15 01:13:07 +0530

    Next Page navigation

--- python/scrapy/r_earthporn/r_earthporn/spiders/Posts.py
+++ python/scrapy/r_earthporn/r_earthporn/spiders/Posts.py
@@ -12,14 +12,18 @@ class PostsSpider(scrapy.Spider):
         table_xpath = "//div[@id='siteTable']/div"
         href_xpath = "./a/@href"
         title_xpath = "./div[2]/div[@class='top-matter']/p[@class='title']/a/text()"
+        next_xpath = "//div[@class='nav-buttons']/span[@class='nextprev']\
+        /span[@class='next-button']/a/@href"
 
         links = response.xpath(table_xpath).xpath(href_xpath)
+        next_page = response.xpath(next_xpath).extract_first()
 
         for link in links:
             link = link.extract()
             if link.find("/r/") != -1:
                 yield Request(url=response.urljoin(link), callback=self.parse_post)
 
+        # yield Request(url=next_page, callback=self.parse)
 
     def parse_post(self, response):
         title_xpath = "//div[@class='top-matter']/p[@class='title']/a/text()"
