1efab4ef09857f8e23cca87360f937983244545a
GreenJoey <esyray15@gmail.com>
2017-12-03 13:13:42 +0530

    Fixed the moving issue

--- python/scrapy/BorutoScrapper/BorutoScrapper/pipelines.py
+++ python/scrapy/BorutoScrapper/BorutoScrapper/pipelines.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from scrapy.pipelines.images import ImagesPipeline
+from .settings import IMAGES_STORE
 import os
+from shutil import move
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -11,16 +13,19 @@ class BorutoscrapperPipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
         # iterate over the local file paths of all downloaded images
         for result in [x for ok, x in results if ok]:
-            path = result['path']
+            path = os.path.join(IMAGES_STORE, result['path'])
             # here we create the session-path where the files should be in the end
             # you'll have to change this path creation depending on your needs
-            target_path = os.path.join(item['folder_path'], os.path.basename(path))
+            target_path = os.path.join(IMAGES_STORE, item['folder_path'])
 
             if not os.path.isdir(target_path):
                 os.makedirs(target_path)
 
+            print(os.path.isdir(target_path), os.path.isdir(path))
+
             # try to move the file and raise exception if not possible
-            if not os.rename(path, target_path):
+            # if not os.rename(path, target_path):
+            if not move(path, target_path):
                 raise ImageException("Could not move image to target folder")
 
             # here we'll write out the result with the new path,
@@ -30,4 +35,3 @@ class BorutoscrapperPipeline(ImagesPipeline):
                 item[self.IMAGES_RESULT_FIELD].append(result)
 
         return item
-
