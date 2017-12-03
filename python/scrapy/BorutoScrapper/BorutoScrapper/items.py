9ac81773cb8e6ab92f041169ecce321161a373af
GreenJoey <esyray15@gmail.com>
2017-12-03 12:41:21 +0530

    Created the item for downloading

--- python/scrapy/BorutoScrapper/BorutoScrapper/items.py
+++ python/scrapy/BorutoScrapper/BorutoScrapper/items.py
@@ -10,5 +10,5 @@ import scrapy
 
 class BorutoscrapperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    folder_path = scrapy.Field()
+    image_urls = scrapy.Field()
