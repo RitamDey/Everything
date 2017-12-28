96a8a2a2546437c3d9aeb9eb5497e05f753e6caf
GreenJoey <esyray15@gmail.com>
2017-12-28 17:31:36 +0530

    Completed the interquartile problem

--- python/CP/HackerRank/s10-interquartile.py
+++ python/CP/HackerRank/s10-interquartile.py
@@ -1,45 +1,33 @@
-def quartile(arr , length):
-    if length%2 == 1:
-        return arr[(length//2)]  # Since the int divison floors the value
-    else:
+def median(arr, length):
+    if length%2 == 0:
         x1 = length//2
-        x2 = x1-1 
+        x2 = x1 - 1
         return (arr[x1]+arr[x2])/2
+    else:
+        return arr[length//2]
 
 
-def split(arr, length, pivot):
-    lower = []
-    lower_length = 0
-    upper = []
-    upper_length = 0
-    pivot = int(pivot)
-
-    for i in arr:
-        if i < pivot:
-            lower.append(i)
-            lower_length += 1
-        elif i > pivot:
-            upper.append(i)
-            upper_length += 1
-
-    return lower, lower_length, upper, upper_length
+_ = input()
+a = map(int, input().split())
+f = map(int, input().split())
+arr = []
+length = 0
 
 
-if __name__ == '__main__':
-    int(input())
-    x = map(int, input().split())
-    f = map(int, input().split())
-    arr = []
-    length = 0
-    for i, j in zip(x, f):
-        arr += [i]*j
-        length += j
+for i,j in zip(a, f):
+    arr += [i]*j
+    length += j
 
-    arr = sorted(arr)
-    q2 = quartile(arr, length)
-    lower, l_length, upper, u_length = split(arr, length, q2)
-    q1 = quartile(lower, l_length)
-    q3 = quartile(upper, u_length)
+arr = sorted(arr)
+lower, upper = [], []
 
-    print(abs(q1-q3))
+if length%2 == 0:
+    lower = arr[:(length//2)]
+    upper = arr[(length//2):]
+else:
+    lower = arr[:(length//2)+1]
+    upper = arr[(length//2)+1:]
+
+
+print("%.1f" %abs(median(lower, len(lower)) - median(upper, len(upper))))
 
