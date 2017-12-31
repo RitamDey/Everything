541efe3b0de75bf1f04dcd32e005fc88294d9d3e
GreenJoey <esyray15@gmail.com>
2017-12-31 20:35:42 +0530

    Implemented logging

--- python/scraping/Unsplash/spider.py
+++ python/scraping/Unsplash/spider.py
@@ -4,32 +4,37 @@ from io import BytesIO
 from urllib.robotparser import RobotFileParser
 from json import load
 from os.path import exists
+import logging
 
 
 robots = RobotFileParser()
 robots.set_url("https://www.unsplash.com/robots.txt")
 robots.read()
+logging.basicConfig(
+            format="%(asctime)s %(message)s",
+            datefmt="%Y-%m-%d %I:%M:%S %p",
+            level=logging.DEBUG
+        )
 
 
 start_url = "https://unsplash.com/napi/feeds/home"
-seen_photos = set()
 next_page = start_url
 with open("/home/stux/headers.json") as fout:
     headers = load(fout)
 
 
 while next_page:
-    print("Fetching", next_page)
+    logging.debug("Fetching %s" %next_page)
     data = requests.get(next_page, headers=headers).json()
     next_page = data.get('next_page', None)
 
     for pic in data.get('photos', []):
-        if pic['id'] in seen_photos or exists(pic['id']+".jpeg"):
+        if exists(pic['id']+".jpeg"):
+            logging.info("%s already exists" %(pic['id']+".jpeg"))
             continue
-        print("Fetching", pic['id'])
+        logging.debug("Fetching %s" %pic['id'])
         pic_data =  requests.get(pic['urls']['full'])
         pic_data = Image.open(BytesIO(pic_data.content))
-        print("Saving picture", pic['id'])
+        logging.info("Saving picture %s" %(pic['id']+".jpeg"))
         pic_data.save(pic['id']+".jpeg", "JPEG")
-        seen_photos.add(pic['id'])
 
