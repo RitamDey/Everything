35fcebbd423cad0dc4bb7771be96626a3eabf6d4
GreenJoey <esyray15@gmail.com>
2017-12-31 22:08:09 +0530

    Added a item for Quotes

--- python/scrapy/AdvancedScrapy/AdvancedScraping/AdvancedScraping/items.py
+++ python/scrapy/AdvancedScrapy/AdvancedScraping/AdvancedScraping/items.py
@@ -12,3 +12,10 @@ class AdvancedscrapingItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class QuoteItem(scrapy.Item):
+    quote = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
+
