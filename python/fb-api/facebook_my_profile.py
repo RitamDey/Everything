cdb7ed350afad31b70f75d21fe4f5d88dd14f69f
GreenJoey <esyray15@gmail.com>
2017-12-31 14:03:38 +0530

    Remove useless folders and files

--- python/fb-api/facebook_my_profile.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-import facebook
-
-token = os.environ.get('FACEBOOK_TEMP_TOKEN')
-graph = facebook.GraphAPI(token)
-profile = graph.get_object('me', fields='name, location')
-
-print(json.dumps(profile, indent=4))
