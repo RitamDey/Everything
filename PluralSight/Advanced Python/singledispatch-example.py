37f30f6d2ae3917e6a523ae3eb4d3ba3bddbcafa
GreenJoey <esyray15@gmail.com>
2018-01-17 20:34:30 +0530

    Simple of functools.singledispatch in Python

---
+++ PluralSight/Advanced Python/singledispatch-example.py
@@ -0,0 +1,60 @@
+# Example of function overloading with functools.singledispatch
+from functools import singledispatch
+
+
+class Shape:
+    def __init__(self, solid):
+        self.solid = solid
+
+
+class Circle(Shape):
+    def __init__(self, center, radius, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.center = center
+        self.radius = radius
+
+
+class Parallelogram(Shape):
+    def __init__(self, pa, pb, pc, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pa = pa
+        self.pb = pb
+        self.pc = pc
+
+
+class Triangle(Shape):
+    def __init__(self, pa, pb, pc, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pa = pa
+        self.pb = pb
+        self.pc = pc
+
+
+@singledispatch
+def draw(shape):
+    raise TypeError("Don't know how to draw {!r}".format(shape))
+
+
+@draw.register(Circle)
+def circle(shape):
+    print("\u25CF" if shape.solid else "\u25A1")
+
+
+@draw.register(Parallelogram)
+def parallelogram(shape):
+    print("\u25B0" if shape.solid else "\u25B1")
+
+
+@draw.register(Triangle)
+def _(shape):
+    print("\u25B2" if shape.solid else "\u25B3")
+
+
+if __name__ == '__main__':
+    shapes = [Circle(center=(0, 0), radius=5, solid=False),
+            Parallelogram(pa=(0, 0), pb=(2, 0), pc=(1, 1), solid=False),
+            Triangle(pa=(0, 0), pb=(1, 2), pc=(2, 0), solid=True)]
+    
+    for shape in shapes:
+        draw(shape)
+
