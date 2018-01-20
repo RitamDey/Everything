e73ed932331e6da5e3b4190a49621b9add7b2171
GreenJoey <esyray15@gmail.com>
2018-01-20 20:28:40 +0530

    Added capabilty to resume from last seen url

--- python/scrapy/reddit_scrapers/reddit_scrapers/spiders/AggPosts.py
+++ python/scrapy/reddit_scrapers/reddit_scrapers/spiders/AggPosts.py
@@ -2,11 +2,26 @@
 import scrapy
 from scrapy.http import Request
 from reddit_scrapers.items import ImageItem
+from os import path
+from reddit_scrapers.settings import IMAGES_STORE as img_path
 
 
 class PostsSpider(scrapy.Spider):
     name = 'AggPosts'
+    last_url = ''
     start_urls = ['https://www.reddit.com/r/EarthPorn+ExposurePorn+ImaginaryLandscapes+ImaginaryTechnology+LightGraffiti+SkyPorn+futureporn+lightpainting+wallpaper+wallpapers/']
+
+    def start_requests(self):
+        if path.exists(img_path+"/.lastseen") and path.isfile(img_path+"/.lastseen"):
+                with open(img_path+"/.lastseen") as fin:
+                    yield Request(fin.readline().strip())
+        else:
+            for url in self.start_urls:
+                yield Request(url)
+
+    def closed(self, reason):
+        with open(img_path+"/.lastseen", 'w') as fout:
+            fout.write(self.last_url)
 
     def parse(self, response):
         table_xpath = "//div[@id='siteTable']/div"
@@ -24,12 +39,13 @@ class PostsSpider(scrapy.Spider):
             title = title.extract()
             if link.find("/r/") != -1:
                 yield Request(url=response.urljoin(link), callback=self.parse_post)
-            elif link.find("cdnb.artstation.com") != -1 or link.find("cdna.artstation.com") or link.find("i.imgur.com") != -1:
+            elif link.find("cdnb.artstation.com") != -1 or link.find("cdna.artstation.com") != -1 or link.find("i.imgur.com") != -1:
                 image = ImageItem()
                 image["name"] = title
                 image["image_urls"] = [link,]
                 yield image
 
+        self.last_url = response.url
         yield Request(url=next_page, callback=self.parse)
 
     def parse_post(self, response):
