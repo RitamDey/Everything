2a4e6c153a7583cfda704b73468eed20ed964982
GreenJoey <esyray15@gmail.com>
2018-01-28 21:11:19 +0530

    Example of winforms hello world using C#

---
+++ c-sharp/winforms-hello-world.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows.Forms;
+
+
+public class HelloWorld : Form {
+    static public void Main() {
+        Application.Run(new HelloWorld());
+    }
+
+    public HelloWorld() {
+        Text = "Hello Mono World";
+    }
+}
