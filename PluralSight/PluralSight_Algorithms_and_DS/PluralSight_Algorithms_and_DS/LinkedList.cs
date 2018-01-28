7000f9c4af7dba4c1d384fc53524b3233d06f8e4
GreenJoey <esyray15@gmail.com>
2018-01-28 21:11:38 +0530

    ..

---
+++ PluralSight/PluralSight_Algorithms_and_DS/PluralSight_Algorithms_and_DS/LinkedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LinkedList {
+	/// <summary>
+	/// A node in the LinkedList
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class LinkedListNode<T> {
+		/// <summary>
+		/// Constructs a new node with a specified value.
+		/// </summary>
+		/// <param name="value"></param>
+		public LinkedListNode(T value) {
+			Value = value;
+		}
+
+		/// <summary>
+		/// The node value
+		/// </summary>
+		public T Value { get; set; }
+
+		/// <summary>
+		/// The next node in the Linked List (null if last node)
+		/// </summary>
+		public LinkedListNode<T> Next { get; set; }
+	}
+}
