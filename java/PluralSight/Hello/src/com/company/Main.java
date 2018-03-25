e4e2f25d6afcc159dfd1decbc35b2ac3af95018b
GreenJoey <esyray15@gmail.com>
2018-03-25 14:53:58 +0530

    A basic Calculator using logical operators

--- java/PluralSight/Hello/src/com/company/Main.java
+++ java/PluralSight/Hello/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        double val1 = 100.0d;
+        double val2 = 50.0d;
+        double result;
+        char opCode = 'd';
+
+        if(opCode == 'a')
+            result = val1 + val2;
+
+        else if (opCode == 's')
+            result = val1 - val2;
+
+        else if (opCode == 'd')
+            result = val2 != 0 ? val1 / val2 : 0.0d;
+
+        else if (opCode == 'm')
+            result = val1 * val2;
+
+        else {
+            System.out.println("Error - invalid opCode");
+            result = 0.0d;
+        }
+
+        System.out.print(result);
     }
 }
