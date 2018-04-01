fd7476a7797b3fc502df5b3c7d7b39b42eb33724
GreenJoey <esyray15@gmail.com>
2018-04-01 22:48:04 +0530

    Updated the regex and getting the format by a different regex

--- Automation Scripts/rename-pluralsight.py
+++ Automation Scripts/rename-pluralsight.py
@@ -10,11 +10,13 @@ if len(argv) < 2:
 
 
 def move(direc):
+    print(os.listdir(direc))
     for vid in os.listdir(direc):
-        mod_chap = findall(r"mo?d?\d+\-?\w*\-\d+\.\w{3}", vid)
+        format = findall(r"\.\w{3}", vid)
+        mod_chap = findall(r"mo?d?\d+\-?.*\-\d+", vid)
         if mod_chap:
             print(f"Moving {vid} -> {mod_chap}")
-            shutil.move(direc+"/"+vid, direc+"/"+mod_chap[0])
+            shutil.move(direc+"/"+vid, direc+"/"+mod_chap[0]+format[0])
 
 for direc in argv[1:]:
     move(direc)
