2f2ac991128f30b3cb8a43b399648dea06ca31ff
DeadGamer <esyray15@gmail.com>
2018-07-03 22:24:34 +0530

    Added blank Activity class for 2 dice mode. Completed the initial design for 2 dice  mode. Also added Manifests for activity navigation. Also added string resources for the dice descriptions
    Signed off by sTux <esyray15@gmail.com>
    
    Signed-off-by: GreenJoey <esyray15@gmail.com>

---
+++ Google-Applied-CS-Skills/ScarnesDice/app/src/main/java/com/stux/open/scarnesdice/TwoDiceActivity.java
@@ -0,0 +1,20 @@
+package com.stux.open.scarnesdice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TwoDiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_dice);
+    }
+
+    public void roll_click (View view) {}
+
+    public void hold_click (View view) {}
+
+    public void reset_click (View view) {}
+}
