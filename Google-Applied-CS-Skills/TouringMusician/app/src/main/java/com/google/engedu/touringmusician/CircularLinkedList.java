d1545b87be8e8c51470d6f5160c20477f195573b
GreenJoey <esyray15@gmail.com>
2018-07-21 19:13:21 +0530

    Starter code:

---
+++ Google-Applied-CS-Skills/TouringMusician/app/src/main/java/com/google/engedu/touringmusician/CircularLinkedList.java
@@ -0,0 +1,114 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.engedu.touringmusician;
+
+
+import android.graphics.Point;
+
+import java.util.Iterator;
+
+public class CircularLinkedList implements Iterable<Point> {
+
+    private class Node {
+        Point point;
+        Node prev, next;
+        /**
+         **
+         **  YOUR CODE GOES HERE
+         **
+         **/
+    }
+
+    Node head;
+
+    public void insertBeginning(Point p) {
+        /**
+         **
+         **  YOUR CODE GOES HERE
+         **
+         **/
+    }
+
+    private float distanceBetween(Point from, Point to) {
+        return (float) Math.sqrt(Math.pow(from.y-to.y, 2) + Math.pow(from.x-to.x, 2));
+    }
+
+    public float totalDistance() {
+        float total = 0;
+        /**
+         **
+         **  YOUR CODE GOES HERE
+         **
+         **/
+        return total;
+    }
+
+    public void insertNearest(Point p) {
+        /**
+         **
+         **  YOUR CODE GOES HERE
+         **
+         **/
+    }
+
+    public void insertSmallest(Point p) {
+        /**
+         **
+         **  YOUR CODE GOES HERE
+         **
+         **/
+    }
+
+    public void reset() {
+        head = null;
+    }
+
+    private class CircularLinkedListIterator implements Iterator<Point> {
+
+        Node current;
+
+        public CircularLinkedListIterator() {
+            current = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (current != null);
+        }
+
+        @Override
+        public Point next() {
+            Point toReturn = current.point;
+            current = current.next;
+            if (current == head) {
+                current = null;
+            }
+            return toReturn;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        return new CircularLinkedListIterator();
+    }
+
+
+}
