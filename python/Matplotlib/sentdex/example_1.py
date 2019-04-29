d244bc189729e6c82526f259cdeff9ed7b754858
Ritam <esyray15@gmail.com>
2019-04-29 22:00:43 +0530

    Remove useless files

--- python/Matplotlib/sentdex/example_1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from numpy.random import randint, random
-
-
-size = randint(10)
-x = [random() for _ in range(size)]
-y = [random() for _ in range(size)]
-plt.plot(x, y)
-plt.show()
