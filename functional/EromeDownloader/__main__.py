c44b8aebe88f86f1339210a433e562da72e28d6e
Ritam <esyray15@gmail.com>
2019-09-11 00:36:15 +0530

    Added the downloader code

--- functional/EromeDownloader/__main__.py
+++ functional/EromeDownloader/__main__.py
@@ -3,7 +3,9 @@ import requests
 from argparse import ArgumentParser
 from pathlib import Path
 from os import chdir
+from sys import exit
 import pprint
+import subprocess
 
 parser = ArgumentParser()
 parser.add_argument("url", type=str, help="Url of the Erome Playlist")
@@ -12,17 +14,34 @@ arguments = parser.parse_args()
 
 if arguments.folder.exists() and arguments.folder.is_dir():
     chdir(arguments.folder)
+else:
+    print("Destination not found")
+    exit(1)
 
 page = requests.get(arguments.url).text
 etree = etree_fromstring(page)
 video = {}
 
 for videos in etree.xpath("//div[@class='media-group']"):
-    name = videos.xpath("./div[@class='media-details']/h2/text()")[0]
-    video_url = videos.xpath("./div[@class='video']/video/source/@src")[0]
+    name = videos.xpath("./div[@class='media-details']/h2/text()")
+    if name:
+        name = name[0]  # Get the first entry, which should be the media name
+    else:
+        name = videos.xpath("./@id")[0]
+    
+    # Check for the avaliable for format and download the highest one
     video_format = videos.xpath("./div[@class='video']/video/source/@res")[0]
+    video_url = ""
+    if "1080" in video_format:
+        video_url = videos.xpath("./div[@class='video']/video/source[@res=1080]/@src")[0]
+    else:
+        video_url = videos.xpath("./div[@class='video']/video/source/@src")[0]
+
     video[name] = {}
     video[name]["url"] = video_url
     video[name]["format"] = video_format
 
 pprint.pprint(video)
+
+for vid in video:
+    subprocess.call(["/usr/bin/wget", "-O", vid+".mp4", video[vid]["url"]])
