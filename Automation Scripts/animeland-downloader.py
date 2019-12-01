80fff28f68dab9d7dd33be764186a89489279b7e
Ritam <esyray15@gmail.com>
2019-12-01 13:46:58 +0530

    Figured out the way to use with wget

--- Automation Scripts/animeland-downloader.py
+++ Automation Scripts/animeland-downloader.py
@@ -1,35 +1,9 @@
 from selenium.webdriver import Chrome
 from sys import argv, exit
 from time import sleep
-from os import mkdir, chdir
-from os import path
+from os import mkdir, chdir, path
+from subprocess import call
 
-
-def waitUntilDownloadCompleted(driver, maxTime=600):
-    # Download wait function, copied from stackoverflow: https://stackoverflow.com/a/56569251
-    driver.execute_script("window.open()")
-    # switch to new tab
-    driver.switch_to.window(driver.window_handles[-1])
-    # navigate to chrome downloads
-    driver.get('chrome://downloads')
-    # define the endTime
-    endTime = time.time() + maxTime
-    while True:
-        try:
-            # get the download percentage
-            downloadPercentage = driver.execute_script(
-                "return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value")
-            # check if downloadPercentage is 100 (otherwise the script will keep waiting)
-            if downloadPercentage == 100:
-                # exit the method once it's completed
-                return downloadPercentage
-        except:
-            pass
-        # wait for 1 second before checking the percentage next time
-        time.sleep(1)
-        # exit method if the download not completed with in MaxTime.
-        if time.time() > endTime:
-            break
 
 def processEpisode(urls):
     # Get the episode
@@ -43,15 +17,14 @@ def processEpisode(urls):
         sleep(10)
 
         # Make and change to the new directory
-        link = browser.find_element_by_xpath("//div[@style='float:right;'][2]")
+        link = browser.find_element_by_xpath("//div[@style='float:right;'][2]/a").get_attribute("href")
         
-        print(f"{name} => {link.find_element_by_xpath('./a').get_attribute('href')}")
+        print(f"{name} => {link}")
         sleep(5)
 
-        link.click()
-        waitUntilDownloadCompleted(browser)
-        return
+        call(["wget", "header=", "-O", name + ".mp4", link])
 
+ 
 
 def main(url):
     with Chrome(executable_path="/home/stux/Downloads/chromedriver") as browser:
