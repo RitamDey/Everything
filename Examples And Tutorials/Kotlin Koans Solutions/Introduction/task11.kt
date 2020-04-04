d2f9a248d683eaf7bd46d9872e812ede513eed24
Ritam Dey <deyritam27031999@gmail.com>
2020-04-05 03:40:53 +0530

    SAM conversion koan

---
+++ Examples And Tutorials/Kotlin Koans Solutions/Introduction/task11.kt
@@ -0,0 +1,9 @@
+import java.util.Collections
+import java.util.Comparator
+
+
+fun getList(): List<Int> {
+    val arrayList = arrayListOf(1, 5, 2)
+    Collections.sort(arrayList, { x, y -> y - x })
+    return arrayList
+}
