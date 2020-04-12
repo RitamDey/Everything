bd4fa1acc73a87678ca063086e296c079b0a0b85
Ritam Dey <deyritam27031999@gmail.com>
2020-04-12 18:02:33 +0000

    Completed TheSubDB SDK

--- Libraries/thesubdb.py
+++ Libraries/thesubdb.py
@@ -1,19 +1,29 @@
+# TheSubDB Python SDK
+# Copyright (C) 2020  Ritam Dey
 import requests
 from hashlib import md5
 from os import path
 import os
 
 
+class SubtitleNotFound(Exception):
+    pass
+
+
+class SubtitleAlreadyExists(Exception):
+    pass
+
+
 class TheSubDB:
     _URL = "http://api.thesubdb.com"
     _parameters = {}
 
-    def __init__(self, user_agent: str, file_name: str=None):
+    def __init__(self, user_agent: str, file_name: str = None):
         if file_name:
             self.getHash(file_name)
-        self._headers = { "User-Agent": user_agent }
+        self._headers = {"User-Agent": user_agent}
         self._languages = None
-    
+
     @classmethod
     def getSubtitleName(cls, file_name: str) -> str:
         """
@@ -21,7 +31,7 @@ class TheSubDB:
         """
         file_name = file_name.split(".")
         file_name[-1] = "srt"
-        
+
         return ".".join(file_name)
 
     def getHash(self, file_name: str):
@@ -34,21 +44,25 @@ class TheSubDB:
             data = video_file.read(readsize)
             video_file.seek(-readsize, os.SEEK_END)
             data += video_file.read(readsize)
-        
-        video_hash = md5(data).hexdigest()
-        self._parameters = { "hash": video_hash }
 
-    def search(self, get_versions: bool=False):
+        video_hash = md5(data).hexdigest()
+        self._parameters = {"hash": video_hash}
+
+    def search(self, get_versions: bool = False):
         """
         Set-up the parameters and issue a search API call for the calculated hash
         """
         self._parameters["action"] = "search"
         if get_versions:
             self._parameters["versions"] = ""
-        response = requests.get(self._URL, params=self._parameters, headers=self._headers)
+        response = requests.get(
+            self._URL, params=self._parameters, headers=self._headers)
 
         if response.status_code != 200:
-            raise response.raise_for_status()
+            if response.status_code == 404:
+                raise SubtitleNotFound()
+            else:
+                raise response.raise_for_status()
 
         self._clean_headers()
         return response.text
@@ -61,7 +75,8 @@ class TheSubDB:
         self._parameters["action"] = "download"
         self._parameters["language"] = language
 
-        response = requests.get(self._URL, params=self._parameters, headers=self._headers)
+        response = requests.get(
+            self._URL, params=self._parameters, headers=self._headers)
 
         if response.status_code != 200:
             raise response.raise_for_status()
@@ -70,10 +85,24 @@ class TheSubDB:
             subtitle.write(response.text)
 
         self._clean_headers()
-    
-    @classmethod
-    def upload(subtitle_file: str):
-        pass
+
+    def upload(self, subtitle_file: str, video: str):
+        self._parameters["action"] = "upload"
+        with open(subtitle_file, "r") as subtitle_data:
+            request_data = {
+                "file": (subtitle_file, subtitle_data.read()),
+                "hash": self.getHash(video)
+            }
+
+            response = requests.post(
+                self._URL, params=self._parameters, headers=self._headers, files=request_data)
+
+        if response.status_code == 403:
+            raise SubtitleAlreadyExists()
+        elif response.status_code == 415:
+            raise TypeError("Unsupported Media Type")
+        elif response.status_code == 400:
+            raise response.raise_for_status()
 
     def _clean_headers(self):
         if "versions" in self._parameters:
