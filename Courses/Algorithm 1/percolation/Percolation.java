c3433978575652ebdb5daf405e20b5701cb0a765
Ritam Dey <deyritam27031999@gmail.com>
2020-11-24 17:34:45 +0530

    [Coursera][Algorithm 1] Template code
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

---
+++ Courses/Algorithm 1/percolation/Percolation.java
@@ -0,0 +1,33 @@
+/* *****************************************************************************
+ *  Name:              Ritam Dey
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class Percolation {
+    private int n;
+    private int[][] grid;
+
+    Percolation(int n) {
+        /**
+         * Create the n*n grid for the percolation program
+         */
+        this.n = n;
+        this.grid = new int[n][n];
+    }
+
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    public void open(int i, int j) {
+    }
+
+    public boolean percolates() {
+        return false;
+    }
+}
